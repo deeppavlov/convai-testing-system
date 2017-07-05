a8476a63d8a30d3629fabd5aed5f11e649884b1a
bavadim@gmail.com <bavadim@gmail.com>
2017-07-05 17:34:46 +0300

    improve framework

--- src/main/scala/org/pavlovai/telegram/Handler.scala
+++ src/main/scala/org/pavlovai/telegram/Handler.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, ActorLogging}
 import info.mukel.telegrambot4s.actors.ActorBroker
 import info.mukel.telegrambot4s.api._
 import info.mukel.telegrambot4s.api.declarative._
-import info.mukel.telegrambot4s.methods.SendMessage
+import info.mukel.telegrambot4s.methods.{ParseMode, SendMessage}
 import info.mukel.telegrambot4s.models._
 
 /**
@@ -13,21 +13,36 @@ import info.mukel.telegrambot4s.models._
   */
 class Handler(gate: BotBase) extends Actor with ActorLogging {
   import Handler._
-  import info.mukel.telegrambot4s.Implicits._
+  //import info.mukel.telegrambot4s.Implicits._
 
   self ! Initialize
 
   override def receive: Receive = {
     case Initialize =>
 
-    case Update(_, Some(message), _, _, _, _, _, _, _, _) if message.text.getOrElse("null").head == '/' =>
-      gate.request(SendMessage(message.source, "command"))
+    case Command(chatId, "/help") =>
+      gate.request(SendMessage(Left(chatId),
+        """
+          |#TODO
+          |
+          |- write help
+          |- read help
+          |- fix help
+          |
+          |![img](http://vkurselife.com/wp-content/uploads/2016/05/b5789b.jpg)
+        """.stripMargin, Some(ParseMode.Markdown)))
 
     case Update(_, Some(message), _, _, _, _, _, _, _, _) =>
-      gate.request(SendMessage(message.source, message.text.toString))
+      gate.request(SendMessage(Left(message.source), message.text.toString))
   }
 }
 
 object Handler {
   case object Initialize
+
+  private case object Command {
+    def unapply(message: Update): Option[(Long, String)] = {
+      if (message.message.exists(_.text.exists(_.startsWith("/")))) Some((message.message.get.source, message.message.get.text.get)) else None
+    }
+  }
 }
