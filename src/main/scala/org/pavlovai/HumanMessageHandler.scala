196f383a03ec16a28a112d8bc02ef644c8689032
bavadim@gmail.com <bavadim@gmail.com>
2017-07-05 15:22:49 +0300

    implementing framework

--- src/main/scala/org/pavlovai/HumanMessageHandler.scala
+++ src/main/scala/org/pavlovai/HumanMessageHandler.scala
@@ -1,16 +1,20 @@
 package org.pavlovai
 
 import akka.actor.{Actor, ActorLogging, Props}
-import info.mukel.telegrambot4s.models.Update
+import info.mukel.telegrambot4s.api._
+import info.mukel.telegrambot4s.api.declarative._
+import info.mukel.telegrambot4s.methods.{EditMessageReplyMarkup, SendMessage}
+import info.mukel.telegrambot4s.models._
+import info.mukel.telegrambot4s.Implicits._
 
 /**
   * @author vadim
   * @since 04.07.17
   */
-class HumanMessageHandler extends Actor with ActorLogging {
+class HumanMessageHandler(request: RequestHandler) extends Actor with ActorLogging {
   override def receive: Receive = {
-    case u: Update =>
-      log.info(u.toString)
+    case Update(_, Some(message), _, _, _, _, _, _, _, _) =>
+      request(SendMessage(message.source, "olololo"))
   }
 
 }
