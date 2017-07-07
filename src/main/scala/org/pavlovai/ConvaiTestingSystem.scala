f2daeb00d1e1561d7e36b76ce53974a1c76a02ed
bavadim@gmail.com <bavadim@gmail.com>
2017-07-07 15:48:51 +0300

    done with p2p communication

--- src/main/scala/org/pavlovai/ConvaiTestingSystem.scala
+++ src/main/scala/org/pavlovai/ConvaiTestingSystem.scala
@@ -1,9 +1,10 @@
 package org.pavlovai
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorSystem, PoisonPill, Props}
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.Logger
+import org.pavlovai.dialog.{Talk, TalkService}
 import org.pavlovai.telegram.{Bot, TelegramService}
 
 import scala.concurrent.Await
@@ -16,7 +17,8 @@ object ConvaiTestingSystem extends App {
   private val materializer: ActorMaterializer = ActorMaterializer()
   private val logger = Logger(getClass)
 
-  val telegramUserRepo = akkaSystem.actorOf(TelegramService.props, "human-messages-handler")
+  private val telegramService = akkaSystem.actorOf(TelegramService.props, "telegram-service")
+  private val talkService = akkaSystem.actorOf(TalkService.props(telegramService), "talk-service")
 
   private def setting(key: String): Try[String] = Try(conf.getString(key)).orElse {
     logger.error("No configuration for telegram.token found!")
@@ -24,11 +26,13 @@ object ConvaiTestingSystem extends App {
   }
 
   (setting("telegram.token"), setting("telegram.webhook")) match {
-    case (Success(token), Success(webhook)) => new Bot(akkaSystem, materializer, telegramUserRepo, token, webhook).run()
+    case (Success(token), Success(webhook)) => new Bot(akkaSystem, materializer, telegramService, token, webhook).run()
     case _ => logger.error("telegram bot not started, because it not configured! Check config file.")
   }
 
   sys.addShutdownHook {
+    telegramService ! PoisonPill
+    talkService ! PoisonPill
     Await.ready(akkaSystem.terminate(), 30.seconds)
     logger.info("system shutting down")
   }
