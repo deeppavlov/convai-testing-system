13ba82b5bf30bc1ab4a091a51dd006557ec2287a
bavadim@gmail.com <bavadim@gmail.com>
2017-07-18 15:18:02 +0300

    fixing test dialog

--- src/main/scala/org/pavlovai/dialog/DialogFather.scala
+++ src/main/scala/org/pavlovai/dialog/DialogFather.scala
@@ -63,6 +63,7 @@ class DialogFather(gate: ActorRef, protected val textGenerator: ContextQuestions
       }
 
     case CreateTestDialogWithBot(owner, botId) =>
+      log.info("!!!!!!")
       (for {
         txt <- textGenerator.selectRandom
         bot = Bot(botId)
