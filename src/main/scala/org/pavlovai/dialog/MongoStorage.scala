30bc3fe1880aa3068d6c59a17ad1bae7fddfe21e
bavadim@gmail.com <bavadim@gmail.com>
2017-07-19 18:52:17 +0300

    swich off dead bots

--- src/main/scala/org/pavlovai/dialog/MongoStorage.scala
+++ src/main/scala/org/pavlovai/dialog/MongoStorage.scala
@@ -26,7 +26,7 @@ class MongoStorage extends Actor with ActorLogging with ObservableImplicits {
 
   private def initialized(database: MongoDatabase): Receive = {
     case dialog: WriteDialog =>
-      log.info("saving dialog {}", dialog)
+      log.debug("saving dialog {}", dialog)
       val dialogs: MongoCollection[MongoStorage.Dialog] = database.getCollection("dialogs")
       dialogs.insertOne(MongoStorage.Dialog(dialog)).toFuture.onComplete {
         case Failure(e) => log.error("dialog NOT saved: {}", e)
