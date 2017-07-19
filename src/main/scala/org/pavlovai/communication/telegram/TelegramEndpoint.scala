2431097f242feff48f5e31bb067f22d930891cf3
bavadim@gmail.com <bavadim@gmail.com>
2017-07-19 11:47:44 +0300

    adding emojies

--- src/main/scala/org/pavlovai/communication/telegram/TelegramEndpoint.scala
+++ src/main/scala/org/pavlovai/communication/telegram/TelegramEndpoint.scala
@@ -94,9 +94,9 @@ class TelegramEndpoint(daddy: ActorRef) extends Actor with ActorLogging with Sta
 
       telegramCall(SendMessage(Left(id), text, Some(ParseMode.Markdown), replyMarkup = Some(
         InlineKeyboardMarkup(Seq(Seq(
-          InlineKeyboardButton.callbackData("- 0 -",  encodeCallback(dialogId, text, None)),
+          InlineKeyboardButton.callbackData("\u2754",  encodeCallback(dialogId, text, None)),
           InlineKeyboardButton.callbackData("\uD83D\uDC4D", encodeCallback(dialogId, text, Some("bot"))),
-          InlineKeyboardButton.callbackData("human", encodeCallback(dialogId, text, Some("human")))
+          InlineKeyboardButton.callbackData("\uD83D\uDC4E", encodeCallback(dialogId, text, Some("human")))
         ))
         ))))
 
