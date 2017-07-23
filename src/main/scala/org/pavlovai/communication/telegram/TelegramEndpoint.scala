dd73ea83ccbdf029447a7d2d7f22412bf787bd35
bavadim@gmail.com <bavadim@gmail.com>
2017-07-23 16:49:15 +0300

    change help message

--- src/main/scala/org/pavlovai/communication/telegram/TelegramEndpoint.scala
+++ src/main/scala/org/pavlovai/communication/telegram/TelegramEndpoint.scala
@@ -175,19 +175,21 @@ class TelegramEndpoint(daddy: ActorRef) extends Actor with ActorLogging with Sta
 
   private def helpMessage(chatId: Long) = SendMessage(Left(chatId),
     """
-      |1. To start a dialog type or choose a ```/start``` command.
-      |2. You will be connected to a peer or, if no peer is available at the moment, you’ll receive the message from @ConvaiBot 'Please wait for you peer.'
-      |3. Peer might be a bot or another human evaluator.
-      |4. After you were connected with your peer you will receive a starting message - a passage or two from a Wikipedia article.
-      |5. Your task is to discuss the content of a presented passage with the peer and score her/his replies.
-      |6. Please score every utterance of your peer with a 'thumb UP' button if you like it, and 'thumb DOWN' button in the opposite case.
-      |To finish the conversation type or choose a command ```/end```.
-      |7. When the conversation is finished, you will receive a request from @ConvaiBot to score the overall quality of the dialog along three dimensions:
+      |1. Please set your Username in Settings menu.
+      |2. After you have set your username add @ConvaiBot - this is official bot for our competition. ([How to add peer in Telegram](https://telegram.org/faq#q-how-do-i-invite-my-friends))
+      |3. To start a dialog type or choose a `/begin` command .
+      |4. You will be connected to a peer or, if no peer is available at the moment, you’ll receive the message from @ConvaiBot `Please wait for you peer.`.
+      |5. Peer might be a bot or another human evaluator.
+      |6. After you were connected with your peer you will receive a starting message - a passage or two from a Wikipedia article.
+      |7. Your task is to discuss the content of a presented passage with the peer and score her/his replies.
+      |8. Please score every utterance of your peer with a ‘thumb UP’ button if you like it, and ‘thumb DOWN’ button in the opposite case.
+      |9. To finish the conversation type or choose a command `/end`.
+      |10. When the conversation is finished, you will receive a request from @ConvaiBot to score the overall quality of the dialog along three dimensions:
       | - quality - how much are you satisfied with the whole conversation?
       | - breadth - in your opinion was a topic discussed thoroughly or just from one side?
       | - engagement - was it interesting to participate in this conversation?
-      |8. If your peer ends the dialog before you, you will also receive a scoring request from @ConvaiBot.
-      |9. Your conversations with a peer will be recorded for further use. By starting a chat you give permission for your anonymised conversation data to be released publicly under [Apache License Version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
+      |11. If your peer ends the dialog before you, you will also receive a scoring request from @ConvaiBot.
+      |12. Your conversations with a peer will be recorded for further use. By starting a chat you give permission for your anonymised conversation data to be released publicly under [Apache License Version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
     """.stripMargin, Some(ParseMode.Markdown), replyMarkup = Some(ReplyKeyboardRemove()))
 
   private def encodeCbData(messageId: Int, text: String) = s"$messageId,$text"
