727d9d7364f31c0a81b16a806300eb47bd716ad3
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-07-24 19:20:31 +0300

    Add Alice-Bob anonymizer

---
+++ utils/ab.py
@@ -0,0 +1,13 @@
+import sys
+import json
+import random
+lines = sys.stdin.readlines()
+for line in lines:
+    d = json.loads(line)
+    a_index = random.randint(0, 1)
+    b_index = 1 - a_index
+    for t in d['thread']:
+        t['userId'] = "Alice" if t['userId'] == d['users'][a_index]['id'] else "Bob"
+    d['users'][a_index]['id'] = "Alice"
+    d['users'][b_index]['id'] = "Bob"
+    print(json.dump(d))
