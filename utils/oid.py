860b83aa1edc9a4a7a7c4c84ade4ba70f90473f1
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-07-24 21:31:49 +0300

    Fixup error with timezones

--- utils/oid.py
+++ utils/oid.py
@@ -4,16 +4,17 @@
 from bson.objectid import ObjectId
 import datetime
 import sys
+from dateutil.tz import tzutc, tzlocal
  
 if len(sys.argv) != 2:
     print("example: oid.py <days>", file=sys.stderr)
     sys.exit(1)
 
-now = datetime.datetime.now()
+now = datetime.datetime.now(tzlocal())
 yesterday = now - datetime.timedelta(days=int(sys.argv[1]))
-start_date = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0)
-end_date = datetime.datetime(now.year, now.month, now.day, 0, 0, 0)
-oid_start = ObjectId.from_datetime(start_date)
-oid_stop = ObjectId.from_datetime(end_date)
+start_date = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0, 0, tzlocal())
+end_date = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond, tzlocal())
+oid_start = ObjectId.from_datetime(start_date.astimezone(tzutc()))
+oid_stop = ObjectId.from_datetime(end_date.astimezone(tzutc()))
  
-print('{ "_id" : { "$gte" : { "$oid": "%s" }, "$lt" : { "$oid": "%s" } } }' % ( str(oid_start), str(oid_stop) ))
+print('{ "_id" : { "$gte" : { "$oid": "%s" }, "$lt" : { "$oid": "%s" } } }' % (str(oid_start), str(oid_stop)))
