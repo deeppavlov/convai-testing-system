dac77f3125a966d3d309e08b2b9e7c2fc7375288
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-07-24 19:30:29 +0300

    Add script to clean evaluations and user types

---
+++ utils/clean.py
@@ -0,0 +1,13 @@
+import sys
+import json
+lines = sys.stdin.readlines()
+for line in lines:
+    d = json.loads(line)
+    d.pop('evaluation')
+    d['users'][0].pop('username')
+    d['users'][1].pop('username')
+    d['users'][0].pop('userType')
+    d['users'][1].pop('userType')
+    for t in d['thread']:
+        t.pop('evaluation')
+    print(json.dumps(d))
