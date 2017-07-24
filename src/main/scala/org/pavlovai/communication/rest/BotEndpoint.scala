500008c9c68d56067725a95451036cae51e8843d
bavadim@gmail.com <bavadim@gmail.com>
2017-07-24 16:56:08 +0300

    waiting off

--- src/main/scala/org/pavlovai/communication/rest/BotEndpoint.scala
+++ src/main/scala/org/pavlovai/communication/rest/BotEndpoint.scala
@@ -40,7 +40,7 @@ class BotEndpoint(daddy: ActorRef, clock: Clock) extends Actor with ActorLogging
 
   private val waitedMessages = mutable.Set.empty[(ActorRef, Dialog.PushMessageToTalk, Deadline)]
 
-  context.system.scheduler.schedule(1.second, 1.second) { self ! SendMessages }
+  //context.system.scheduler.schedule(1.second, 1.second) { self ! SendMessages }
 
   override def receive: Receive = {
     case GetMessages(token) =>
@@ -59,20 +59,22 @@ class BotEndpoint(daddy: ActorRef, clock: Clock) extends Actor with ActorLogging
 
     case SendMessage(token, chat, m: BotMessage) =>
       activeChats.get(Bot(token) -> chat).foreach{ to =>
-        val typeTime = rnd.nextGaussian() * 5 + m.text.length / 2
-        val typeTimeTrunc = if (typeTime > 60) 60 else if (typeTime < 5) 5 else typeTime
-        log.debug("slowdown message delivery from bot on {} seconds", typeTimeTrunc)
-        waitedMessages.add((to, Dialog.PushMessageToTalk(Bot(token), m.text), Deadline.now + typeTimeTrunc.seconds))
+        //val typeTime = rnd.nextGaussian() * 5 + m.text.length / 2
+        //val typeTimeTrunc = if (typeTime > 60) 60 else if (typeTime < 5) 5 else typeTime
+        //log.debug("slowdown message delivery from bot on {} seconds", typeTimeTrunc)
+        //waitedMessages.add((to, Dialog.PushMessageToTalk(Bot(token), m.text), Deadline.now + typeTimeTrunc.seconds))
+
+        to ! Dialog.PushMessageToTalk(Bot(token), m.text)
       }
       sender ! Message(rnd.nextInt(), None, Instant.now(clock).getNano, Chat(chat, ChatType.Private), text = Some(m.toJson(botMessageFormat).toString))
 
-    case SendMessages =>
+    /*case SendMessages =>
       waitedMessages.retain {
         case (to, message, timeout) if timeout.isOverdue() =>
           to ! message
           false
         case _ => true
-      }
+      }*/
 
     case Endpoint.ChatMessageToUser(Bot(token), text, dialogId, _) =>
       botsQueues.get(token).fold[Any] {
@@ -123,5 +125,5 @@ object BotEndpoint extends SprayJsonSupport with DefaultJsonProtocol  {
 
   implicit val sendMessageFormat: JsonFormat[SendMessage] = jsonFormat3(SendMessage)
 
-  private case object SendMessages
+  //private case object SendMessages
 }
