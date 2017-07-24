2b1cc83f9834a27a4ecde0950e9baf30a7273465
bavadim@gmail.com <bavadim@gmail.com>
2017-07-24 20:12:53 +0300

    leaderboard complete

--- utils/leaderboard.py
+++ utils/leaderboard.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 
-import roc_auc_score from sklearn.metrics
-import numpy as np
-import pandas as pd
+from sklearn.metrics import roc_auc_score
 import sys
 import json
+import csv
 
-if len(sys.argv) != 3:
-    print("example: leaderboard.py <dataset.json> <labels.csv>", file=sys.stderr)
+if len(sys.argv) != 2:
+    print("example: leaderboard.py <labels.csv>", file=sys.stderr)
     sys.exit(1)
 
-dataset_file = sys.argv[1]
-labels_file = sys.argv[2]
+lines = sys.stdin.readlines()
 
-df = pd.read_csv(labels_file, header = 0)
-df[[0]]
+users_bot_flags = {}
+for line in lines:
+    d = json.loads(line)
+    if d['users'][0]['id'] == 'Alice':
+        users_bot_flags[d['dialogId']] = ( (int(d['users'][0]['userType'] == "org.pavlovai.communication.Bot"), int(d['users'][1]['userType'] == "org.pavlovai.communication.Bot")) )
+    else:
+        users_bot_flags[d['dialogId']] = ( (int(d['users'][1]['userType'] == "org.pavlovai.communication.Bot"), int(d['users'][0]['userType'] == "org.pavlovai.communication.Bot")) )
+
+users_bot_predicted_probs = []
+users_bot_fact_labaels = []
+with open(sys.argv[1], 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        dialog = int(row[0])
+        if dialog in users_bot_flags:
+            users_bot_fact_labaels.append(users_bot_flags[dialog][0])
+            users_bot_predicted_probs.append(float(row[1]))
+
+            users_bot_fact_labaels.append(users_bot_flags[dialog][1])
+            users_bot_predicted_probs.append(float(row[2]))
+        else:
+            print("dialog " + dialog + " not in dataset", file=sys.stderr)
+
+print(sys.argv[1], ": ", roc_auc_score(users_bot_fact_labaels, users_bot_predicted_probs))
+
