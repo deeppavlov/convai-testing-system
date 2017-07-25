03d74207eafc37154f3c4f54020664af73dc3ac5
bavadim@gmail.com <bavadim@gmail.com>
2017-07-25 19:19:55 +0300

    use spearman correlation in leaderboard

--- utils/leaderboard.py
+++ utils/leaderboard.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sklearn.metrics import roc_auc_score
+from scipy.stats import spearmanr
 import sys
 import json
 import csv
@@ -14,10 +14,10 @@ lines = sys.stdin.readlines()
 users_bot_flags = {}
 for line in lines:
     d = json.loads(line)
-    if d['users'][0]['id'] == 'Alice':
-        users_bot_flags[d['dialogId']] = ( (int(d['users'][0]['userType'] == "org.pavlovai.communication.Bot"), int(d['users'][1]['userType'] == "org.pavlovai.communication.Bot")) )
+    if d['evaluation'][0]['userId'] == 'Alice':
+        users_bot_flags[d['dialogId']] = ( (int(d['evaluation'][0]['quality']), int(d['evaluation'][1]['quality'])) )
     else:
-        users_bot_flags[d['dialogId']] = ( (int(d['users'][1]['userType'] == "org.pavlovai.communication.Bot"), int(d['users'][0]['userType'] == "org.pavlovai.communication.Bot")) )
+        users_bot_flags[d['dialogId']] = ( (int(d['evaluation'][1]['quality']), int(d['evaluation'][0]['quality'])) )
 
 users_bot_predicted_probs = []
 users_bot_fact_labaels = []
@@ -27,12 +27,12 @@ with open(sys.argv[1], 'r') as csvfile:
         dialog = int(row[0])
         if dialog in users_bot_flags:
             users_bot_fact_labaels.append(users_bot_flags[dialog][0])
-            users_bot_predicted_probs.append(float(row[1]))
+            users_bot_predicted_probs.append(int(row[1]))
 
             users_bot_fact_labaels.append(users_bot_flags[dialog][1])
-            users_bot_predicted_probs.append(float(row[2]))
+            users_bot_predicted_probs.append(int(row[2]))
         else:
             print("dialog " + dialog + " not in dataset", file=sys.stderr)
 
-print(sys.argv[1], ": ", roc_auc_score(users_bot_fact_labaels, users_bot_predicted_probs))
+print(sys.argv[1], ": ", spearmanr(users_bot_fact_labaels, users_bot_predicted_probs).correlation)
 
