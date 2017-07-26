a54a33860508db768166a1c22eb2266635908b28
bavadim@gmail.com <bavadim@gmail.com>
2017-07-26 14:17:25 +0300

    correction on urgently distributed users in dialog constructor

--- src/main/scala/org/pavlovai/dialog/DialogConstructionRules.scala
+++ src/main/scala/org/pavlovai/dialog/DialogConstructionRules.scala
@@ -44,13 +44,15 @@ trait DialogConstructionRules {
       }.collect { case (a, b, Success(txt)) => (a, b, txt) }
   }*/
 
+  var urgentlyDistributedK = 0
+
   def availableDialogs(humanBotCoef: Double)(users: List[(User, Int, Deadline)]): Seq[(User, User, String)] = {
     val dedlinedUsers = users.filter(_._3.isOverdue()).map(t => (t._1, t._2))
     val commonUsers = users.filter(_._3.hasTimeLeft()).map(t => (t._1, t._2))
 
     val humans = rnd.shuffle(commonUsers.filter { case (user, _) => user.isInstanceOf[Human] }).map(_._1)
     val robots = mutable.Map(commonUsers.filter { case (user, _) => user.isInstanceOf[Bot] }: _*)
-    val humanProb = 1.0 - 1.0 / (2 * humanBotCoef + 1)
+    val P0 = 1.0 - 1.0 / (2 * humanBotCoef + 1)
 
     def randomRobot() = {
       val (r, count) = rnd.shuffle(robots.filter(_._2 > 0)).head
@@ -60,8 +62,13 @@ trait DialogConstructionRules {
 
     val overdueUsers = dedlinedUsers.filter(d => d._2 > 0).foldRight(List.empty[(User, User)]) { case ((user, capacity), acc) =>
       if (!robots.exists(_._2 > 0) || capacity < 1) acc
-      else (user, randomRobot()) :: acc
+      else {
+        urgentlyDistributedK += 1
+        (user, randomRobot()) :: acc
+      }
     }
+
+    val humanProb = (P0 * Math.exp(urgentlyDistributedK)) / (1 + P0 * (Math.exp(urgentlyDistributedK) - 1))
 
     (humans.zip(humans.reverse).take(humans.length / 2)
       .map { case (u1, u2) => if (u1.id.hashCode < u2.id.hashCode) (u1, u2) else (u2, u1) }
