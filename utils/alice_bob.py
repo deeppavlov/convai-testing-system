2d456f535c45428dae96d405b0261d20787fee1e
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-07-26 11:17:12 +0300

    Swap Alice and Bob evaluations

--- utils/alice_bob.py
+++ utils/alice_bob.py
@@ -8,10 +8,18 @@ for line in lines:
     d = json.loads(line)
     a_index = random.randint(0, 1)
     b_index = 1 - a_index
+
     for t in d['thread']:
         t['userId'] = "Alice" if t['userId'] == d['users'][a_index]['id'] else "Bob"
+
+    """
+    В базе для Alice (Bob) хранятся оценки которые она ПОСТАВИЛА
+    В выгрузке для Alice (Bob) будут выводиться оценки которые она ПОЛУЧИЛА
+    Для этого меняем местами оценки Alice и Bob соотвествено 
+    """
     for e in d['evaluation']:
-        e['userId'] = "Alice" if e['userId'] == d['users'][a_index]['id'] else "Bob"
+        e['userId'] = "Bob" if e['userId'] == d['users'][a_index]['id'] else "Alice"
+
     d['users'][a_index]['id'] = "Alice"
     d['users'][b_index]['id'] = "Bob"
     print(json.dumps(d))
