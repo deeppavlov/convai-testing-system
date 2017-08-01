387ae8ee01d37c739a0d3a02d54c15bde089c6b6
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-08-01 11:40:55 +0300

    Add clean script fo usernames

---
+++ utils/username_clean.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+lines = sys.stdin.readlines()
+for line in lines:
+    d = json.loads(line)
+    d.pop('_id')
+    for u in d['users']:
+        u.pop('username')
+        if u['userType'] == 'org.pavlovai.communication.Bot':
+            u['userType'] = 'Bot'
+        else:
+            u['userType'] = 'Human'
+    print(json.dumps(d))
