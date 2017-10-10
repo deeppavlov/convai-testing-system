a451ad19ea96c310919676abde28240a8a319566
bavadim@gmail.com <bavadim@gmail.com>
2017-10-10 16:07:47 +0300

    improve user error messages

--- src/main/scala/ai/ipavlov/communication/user/Messages.scala
+++ src/main/scala/ai/ipavlov/communication/user/Messages.scala
@@ -7,6 +7,8 @@ package ai.ipavlov.communication.user
 object Messages {
   val notSupported = "Messages of this type aren't supported \uD83D\uDE1E"
 
+  val youCantDoItNow =  "Sorry, you can't to do it now"
+
   val exit = """`(system msg):` exit"""
 
   val lastNotificationInDialog = """Thank you! It was great! Please choose /begin to continue evaluation."""
--- src/main/scala/ai/ipavlov/communication/user/User.scala
+++ src/main/scala/ai/ipavlov/communication/user/User.scala
@@ -1,7 +1,7 @@
 package ai.ipavlov.communication.user
 
 import ai.ipavlov.communication.Endpoint
-import ai.ipavlov.communication.user.User.TryShutdown
+import ai.ipavlov.communication.user.User.{TryShutdown, UserCommand}
 import ai.ipavlov.dialog.{Dialog, DialogFather}
 import akka.actor.{ActorRef, FSM, LoggingFSM, Props}
 
@@ -166,6 +166,10 @@ class User(summary: Human, dialogDaddy: ActorRef, client: ActorRef) extends Logg
   }
 
   whenUnhandled {
+    case Event(_: UserCommand, Uninitialized) =>
+      client ! Client.ShowSystemNotification(summary.address, Messages.youCantDoItNow)
+      stay()
+
     case Event(event, data) =>
       log.warning("Received unhandled event: {} in state {}", event, stateName)
       client ! Client.ShowSystemNotification(summary.address, Messages.notSupported)
