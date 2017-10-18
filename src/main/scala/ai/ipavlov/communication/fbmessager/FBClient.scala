f98ec34d73f1217b59bde55f9ca03dcc01a210c5
bavadim@gmail.com <bavadim@gmail.com>
2017-10-18 16:02:56 +0300

    fix fb help text

--- src/main/scala/ai/ipavlov/communication/fbmessager/FBClient.scala
+++ src/main/scala/ai/ipavlov/communication/fbmessager/FBClient.scala
@@ -71,13 +71,13 @@ class FBClient(pageAccessToken: String) extends Actor with ActorLogging {
         """.stripMargin
       else
         """
-          | 1. To start a dialog type or choose a / begin command.
+          | 1. To start a dialog type or choose a /begin command.
           | 2. You will be connected to a peer or, if no peer is available at the moment, you’ll receive the message "Please wait for you partner".
           | 3. Peer might be a bot or another human evaluator.
           | 4. After you were connected with your peer you will receive a starting message -a passage or two from a Wikipedia article.
-          | 5. Your task is to discuss the content of a presented passage with the peer and score her / his replies.
+          | 5. Your task is to discuss the content of a presented passage with the peer and score her/his replies.
           | 6. Please score every utterance of your peer with a ‘thumb UP’ button if you like it, and ‘thumb DOWN’ button in the opposite case.
-          | 7. To finish the conversation type or choose a command / end.
+          | 7. To finish the conversation type or choose a command /end.
           | 8. When the conversation is finished, you will receive a request to score the overall quality of the dialog along three dimensions:
           | - quality - how much are you satisfied with the whole conversation?
           | - breadth - in your opinion was a topic discussed thoroughly or just from one side?
